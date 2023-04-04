49f1c707324f0741a6c16e453949646f476fab5b
vutuanhai181098 <vutuanhai.18101998@gmail.com>
2023-04-04 12:38:09 +0700

    BTVN

---
+++ ManageHospital/src/ManageHospital.java
@@ -0,0 +1,9 @@
+public class ManageHospital {
+    private Patient patient;
+    private Doctor doctor;
+
+    public ManageHospital(Patient patient, Doctor doctor) {
+        this.patient = patient;
+        this.doctor = doctor;
+    }
+}
